7c236b647045e7ebe7e4656dc0839d482229a925
Ganesh Hubale <ghubale@redhat.com>
2020-08-13 19:09:02 +0530

    - Update for config path - Update README Signed-off-by: Ganesh Hubale <ghubale@redhat.com>
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # jaspro
-Jasmine + Protactor
+Jasmine + Protractor
+
+1. Install npm
+2. Install Nodejs
+3. Install Protractor - https://www.protractortest.org/#/tutorial
--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 exports.config = {
   framework: 'jasmine', // testing framework used
   seleniumAddress: 'http://172.17.0.2:4444/wd/hub', // miqsel tool - remove selenium server
-  specs: ['FirstTest.js'], // test case file
+  specs: ['tests/FirstTest.js'], // test case file
   capabilities: {
-    browserName: 'firefox'
+    browserName: 'chrome'
   }
 };
