3a62361a405c00c3706e506cabac049a67676b41
Ganesh Hubale <ghubale@redhat.com>
2020-08-20 17:01:43 +0530

    Added report creation feature
    Signed-off-by: Ganesh Hubale <ghubale@redhat.com>

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
+var HtmlReporter = require('protractor-beautiful-reporter');
+
 exports.config = {
   framework: 'jasmine', // testing framework used
   seleniumAddress: 'http://localhost:4444/wd/hub',
@@ -8,5 +10,12 @@ exports.config = {
   //{
     //browserName: 'chrome'
   //}
-  ]
+  ],
+
+  onPrepare: function() {
+// Add a screenshot reporter and store screenshots to `/Reports/screenshots`:
+jasmine.getEnv().addReporter(new HtmlReporter({
+         baseDirectory: 'Reports'
+      }).getJasmine2Reporter());
+   }
 };
